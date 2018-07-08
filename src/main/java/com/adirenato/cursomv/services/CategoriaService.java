a688f17a1e3d91f359d1ce476b1af103b7ec6714
adirenato <adir.souza@acad.pucrs.br>
2018-07-08 20:35:00 -0300

    ajuste no endpoint categoria categoria/{id}

--- src/main/java/com/adirenato/cursomv/services/CategoriaService.java
+++ src/main/java/com/adirenato/cursomv/services/CategoriaService.java
@@ -7,15 +7,16 @@ import org.springframework.stereotype.Service;
 
 import com.adirenato.cursomv.domain.Categoria;
 import com.adirenato.cursomv.repositories.CategoriaRepository;
+import com.adirenato.cursomv.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> c = repo.findById(id);
-		return c.orElse(null);
+		//função lambda
+		return c.orElseThrow( () -> new ObjectNotFoundException ("Objeto não encontrado "+ id + ", tipo :"+ CategoriaService.class.getName()) );
 	}
 }
