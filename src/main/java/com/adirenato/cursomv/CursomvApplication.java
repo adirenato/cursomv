2586be16e5961ea3d8b0915486b4346f72ae3712
adirenato <adir.souza@acad.pucrs.br>
2018-07-08 16:36:03 -0300

    produto e associação many to many

--- src/main/java/com/adirenato/cursomv/CursomvApplication.java
+++ src/main/java/com/adirenato/cursomv/CursomvApplication.java
@@ -1,6 +1,5 @@
 package com.adirenato.cursomv;
 
-
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,12 +8,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.adirenato.cursomv.domain.Categoria;
+import com.adirenato.cursomv.domain.Produto;
 import com.adirenato.cursomv.repositories.CategoriaRepository;
+import com.adirenato.cursomv.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomvApplication implements CommandLineRunner {
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	@Autowired
+	ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomvApplication.class, args);
@@ -22,10 +25,23 @@ public class CursomvApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// categorias
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		// produtos
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
 
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 }
